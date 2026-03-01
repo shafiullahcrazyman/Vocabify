59717b413a723c0026efbd8be3c321d33d7c9b0a
Shafiullah <shafiullahcrazyman@gmail.com>
2026-03-01 15:26:00 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'vocabify-cache-v1';
+
+const ASSETS_TO_CACHE = [
+  '/Vocabify/',
+  '/Vocabify/index.html',
+  '/Vocabify/manifest.json',
+  '/Vocabify/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
